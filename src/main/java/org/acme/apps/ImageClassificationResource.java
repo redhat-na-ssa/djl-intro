a23928f1675015ea51ea1ed25ec4f12655de077d
jbride <jbride2001@yahoo.com>
2023-02-01 08:25:47 -0700

    imageclassification: image url is now configurable via application.properties

--- src/main/java/org/acme/apps/ImageClassificationResource.java
+++ src/main/java/org/acme/apps/ImageClassificationResource.java
@@ -31,6 +31,8 @@ import ai.djl.translate.Translator;
 import io.quarkus.arc.lookup.LookupIfProperty;
 import io.smallrye.mutiny.Uni;
 import org.apache.commons.imaging.Imaging;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.core.Response;
 
@@ -44,7 +46,7 @@ import org.jboss.logging.Logger;
 @ApplicationScoped
 public class ImageClassificationResource extends BaseResource implements IApp {
 
-    private static final String IMAGE_URL = "https://djl-ai.s3.amazonaws.com/resources/images/kitten_small.jpg";
+    private static final String KITTEN_IMAGE_URL = "https://djl-ai.s3.amazonaws.com/resources/images/kitten_small.jpg";
 
     private static final Logger log = Logger.getLogger("ImageClassResource");
     private static final float[] MEAN = {103.939f, 116.779f, 123.68f};
@@ -54,56 +56,45 @@ public class ImageClassificationResource extends BaseResource implements IApp {
     private static final String MXNET = "MXNet";
 
     private Image image;
-    private Criteria<Image, Classifications> criteria;
     ZooModel<Image, Classifications> model;
 
+    @ConfigProperty(name = "org.acme.djl.imageclassification.image.url", defaultValue = KITTEN_IMAGE_URL)
+    String imageUrl;
     
     public void startResource()  {
-
+        
         super.start();
-
+        log.info("startResource() image classification based on: "+ imageUrl);
+        
         try {
 
-            URL url = new URL(IMAGE_URL);
+            URL url = new URL(imageUrl);
             try (InputStream is = url.openStream()) {
                 BufferedImage bImage = Imaging.getBufferedImage(is);
-
+                
                 // when ai.djl.opencv:opencv is on classpath;  this factory = ai.djl.opencv.OpenCVImageFactory
                 // otherwise, this factory                                  = ai.djl.modality.cv.BufferedImageFactory
                 ImageFactory iFactory = ImageFactory.getInstance();
                 log.info("ImageFactory = "+iFactory.getClass().getCanonicalName());
-
-
+                
                 image = iFactory.fromImage(bImage);
             }
-        
+            
+            // Whay does TensorFlow need a Translator ??
+            Translator<Image, Classifications> translator = null;
             if (TENSOR_FLOW.equals(engineName)) {
-                Translator<Image, Classifications> translator = ImageClassificationTranslator.builder()
-                        .addTransform(new Resize(224))
-                        .addTransform(new Normalize(MEAN, STD))
-                        .build();
-                criteria = Criteria.builder()
-                                .setTypes(Image.class, Classifications.class) // define input & output
-                                .optArtifactId("resnet")
-                                .optTranslator(translator)
-                                .optProgress(new ProgressBar())
-                                .build();
-            } else if (PYTORCH.equals(engineName)) {
-                criteria = Criteria.builder()
-                                .setTypes(Image.class, Classifications.class) // define input & output
-                                .optArtifactId("resnet")
-                                .optProgress(new ProgressBar())
-                                .optEngine("PyTorch")
-                                .build();
-            } else if (MXNET.equals(engineName)) {
-                criteria = Criteria.builder()
-                    .setTypes(Image.class, Classifications.class) // define input & output
-                    .optArtifactId("resnet")
-                    .optProgress(new ProgressBar())
-                    .build();
-            } else {
-                throw new RuntimeException("Unknown engine type: "+engineName);
+                translator = ImageClassificationTranslator.builder()
+                .addTransform(new Resize(224))
+                .addTransform(new Normalize(MEAN, STD))
+                .build();
             }
+            
+            Criteria<Image, Classifications> criteria = Criteria.builder()
+                .setTypes(Image.class, Classifications.class) // define input & output
+                .optTranslator(translator)
+                .optArtifactId("resnet")
+                .optProgress(new ProgressBar())
+                .build();
     
             model  = ModelZoo.loadModel(criteria);
     
@@ -116,9 +107,12 @@ public class ImageClassificationResource extends BaseResource implements IApp {
 
     public Uni<Response> predict() {
 
-        try (
-            Predictor<Image, Classifications> predictor = model.newPredictor()) {
+        Predictor<Image, Classifications> predictor = null;
+        try {
 
+            // https://djl.ai/docs/development/inference_performance_optimization.html
+            // "DJL Predictor is not designed to be thread-safe (although some implementations may be"
+            predictor = model.newPredictor();
 
             Classifications result = predictor.predict(image);
             Response eRes = Response.status(Response.Status.OK).entity(result.toJson()).build();
@@ -127,6 +121,9 @@ public class ImageClassificationResource extends BaseResource implements IApp {
             e.printStackTrace();
             Response eRes = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
             return Uni.createFrom().item(eRes);
+        } finally {
+            if(predictor != null)
+                predictor.close();
         }
     }       
     
