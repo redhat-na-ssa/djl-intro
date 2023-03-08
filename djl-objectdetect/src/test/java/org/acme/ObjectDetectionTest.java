6d77443c4d021a5b64bc59624c5eeef9fecaeb71
jbride <jbride2001@yahoo.com>
2023-03-08 06:24:54 -0700

    djl-objectdetect: testing

--- djl-objectdetect/src/test/java/org/acme/ObjectDetectionTest.java
+++ djl-objectdetect/src/test/java/org/acme/ObjectDetectionTest.java
@@ -1,16 +1,29 @@
 package org.acme;
 
+
+import static org.awaitility.Awaitility.await;
+
 import javax.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import io.quarkus.test.junit.QuarkusTest;
-
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.jboss.logging.Logger;
 
 /*
@@ -20,30 +33,81 @@ import org.jboss.logging.Logger;
 @QuarkusTest
 public class ObjectDetectionTest {
 
-    private static Logger log = Logger.getLogger("FPrintMainTest");
+    private static Logger log = Logger.getLogger(ObjectDetectionTest.class);
     private static ObjectMapper oMapper = new ObjectMapper();
     private final AtomicInteger mqttCount = new AtomicInteger(0);
+    private static MqttClient client;
 
     @Inject
     ObjectDetectionMain iMain;
+
+    @ConfigProperty(name = "mp.messaging.incoming.liveObjectDetectionIncoming.host")
+    String host;
+
+    @ConfigProperty(name = "mp.messaging.incoming.liveObjectDetectionIncoming.port")
+    String port;
+
+    @ConfigProperty(name = "mp.messaging.incoming.liveObjectDetectionIncoming.username")
+    String user;
+
+    @ConfigProperty(name = "mp.messaging.incoming.liveObjectDetectionIncoming.password")
+    String pwd;
+
+    @ConfigProperty(name = "mp.messaging.incoming.liveObjectDetectionIncoming.topic")
+    String topic;
+
+    @BeforeEach
+    void beforeEach() {
+        if(client == null){
+            try {
+                client = new MqttClient("tcp://" + host + ":" + port, UUID.randomUUID().toString());
+                MqttConnectOptions options = new MqttConnectOptions();
+                options.setUserName(user);
+                options.setPassword(pwd.toCharArray());
+                client.connect(options);
+                options.setKeepAliveInterval(60);
+                await().until(client::isConnected);
+                log.infov("beforeEach() just created mqtt connection to {0}:(1}", host, port);
+
+            } catch (MqttException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    
+    }
     
     @Test
     public void predictionCDITest() {
 
         Integer vCaptureDevice = (Integer)iMain.predict().await().atMost(Duration.ofSeconds(2)).getEntity();
 
-        /*
-        TO-DO:  receive MQTT message and run tests
+        // setup callback
+        client.setCallback(new MqttCallback() {
 
-        while(mqttCount.get() != 1){
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            public void connectionLost(Throwable cause) {
+                log.info("connectionLost: " + cause.getMessage());
+           }
 
+            public void messageArrived(String topic, MqttMessage message) {
+                log.infov("topic: {0}  ;QoS: {1}", topic, message.getQos());
+                log.info("message content: " + new String(message.getPayload()));
+                mqttCount.incrementAndGet();
+           }
+
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                log.info("deliveryComplete---------" + token.isComplete());
+           }
+
+        });
+        try {
+            client.subscribe(topic, 0);
+            await().atMost(5000, TimeUnit.MILLISECONDS).until( () -> mqttCount.get() == 1);
+            Assertions.assertTrue(mqttCount.get() ==1);
+            client.unsubscribe(topic);
+
+        } catch (MqttException e) {
+            e.printStackTrace();
         }
-        */
         
     }
 
